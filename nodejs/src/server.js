e28f4ccab748e9105039c1844d0beec41e736d46
Tran Thieu <thieuhaitam@gmail.com>
2023-03-14 16:39:37 +0700

    nodejs 2-9

---
+++ nodejs/src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const trainingRoute = require('./routes/training.route');
+const authRoute = require('./routes/auth.route');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8080;
+app.use(
+  express.urlencoded({
+    limit: '30mb',
+    extended: true,
+  })
+);
+app.use(
+  express.json({
+    limit: '30mb',
+    extended: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/training', trainingRoute);
+app.use('/auth', authRoute);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
